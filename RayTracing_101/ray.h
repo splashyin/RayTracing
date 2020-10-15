9f33243a42d730f141a7c04c45d322dc364ea5a5
splashyin <yin.hao@rocketmail.com>
2020-10-16 00:17:04 +1300

    Added basic Ray class

---
+++ RayTracing_101/ray.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "vec3.h"
+
+class Ray
+{
+public:
+    // Ctor
+    Ray() {}
+
+    Ray( const point3& i_origin, const vec3& i_direction )
+        : m_origin( i_origin ),
+          m_direction( i_direction )
+    {}
+    
+    // Get origin
+    inline point3 origin() const { return m_origin; }
+
+    // Get direction
+    inline vec3 direction() const { return m_direction; }
+
+    // Ray function P(t) = A + tB
+    inline point3 at( double i_t ) const
+    {
+        return m_origin + t * m_direction;
+    }
+
+    point3 m_origin;
+    vec3 m_direction;
+};
